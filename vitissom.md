bbc36d997fe832e1cdfe699d9d39371fd6c48dbe
t4k <tom4konecny@gmail.com>
2023-11-17 09:58:13 +0400

    update page

--- vitissom.md
+++
@@ -1,3 +0,0 @@
-layout: page
-title: "vitissom"
-permalink: /vitissom
--- vitissom/som/annotation/filter.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-rm filtered_gene_annotation.gff3
-gunzip -kv *.gz
-cat *.gff3 | grep "gene" | grep "Vitvi" | grep "VIT_" > filtered_gene_annotation.gff3
